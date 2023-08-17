064edc888aca7c3fc07f7eb68c2ef2cf2c31335b
Zhao Xin <7176466@qq.com>
2023-08-17 08:43:53 +0800

    v1.2.20

--- p5js-cli.test.js
+++ p5js-cli.test.js
@@ -1,30 +1,31 @@
-const path = require('path')
-const exec = require('child_process').exec
-const sketchName = require('uuid').v4()
+const path = require("path");
+const exec = require("child_process").exec;
+const sketchName = require("uuid").v4();
 
-describe('create new sketch', () => {
-    test('create successfully.', async () => {
-        const result = await cli(['new', sketchName], '.')
-        expect(result.code).toBe(0)
-    })
-    test('failed when exists.', async () => {
-        const result = await cli(['new', sketchName], '.')
-        expect(result.code).toBe(1)
-    })
-})
+describe("create new sketch", () => {
+    test("create successfully.", async () => {
+        const result = await cli(["new", sketchName], "/tmp");
+        expect(result.code).toBe(0);
+    });
+    test("failed when exists.", async () => {
+        const result = await cli(["new", sketchName], "/tmp");
+        expect(result.code).toBe(1);
+    });
+});
 
 function cli(args, cwd) {
-    return new Promise(resolve => {
-        exec(`node ${path.resolve('./p5js-cli')} ${args.join(' ')}`,
+    return new Promise((resolve) => {
+        exec(
+            `node ${path.resolve("./p5js-cli")} ${args.join(" ")}`,
             { cwd },
             (error, stdout, stderr) => {
                 resolve({
                     code: error && error.code ? error.code : 0,
                     error,
                     stdout,
-                    stderr
-                })
+                    stderr,
+                });
             }
-        )
-    })
+        );
+    });
 }
